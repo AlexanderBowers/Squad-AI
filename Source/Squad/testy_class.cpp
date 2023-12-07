542287c96483cc749c0783a7fa98c65aadc7d08f
AlexanderBowers <7319abowers@gmail.com>
2023-12-07 01:08:43 -0800

    testing
    test commit modifying map and adding c++ class.

---
+++ Source/Squad/testy_class.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "testy_class.h"
+
+testy_class::testy_class()
+{
+}
+
+testy_class::~testy_class()
+{
+}
---
+++ Source/Squad/testy_class.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SQUAD_API testy_class
+{
+public:
+	testy_class();
+	~testy_class();
+};
