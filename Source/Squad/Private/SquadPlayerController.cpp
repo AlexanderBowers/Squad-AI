998003e1948228196080b2e58889c854fc405900
AlexanderBowers <7319abowers@gmail.com>
2023-12-09 01:34:51 -0800

    Still struggling with MoveUpCommand.
    Compile errors with variable declaration. Hoping DrawDebugPoint will work on tick.

--- Source/Squad/Private/SquadPlayerController.cpp
+++ Source/Squad/Private/SquadPlayerController.cpp
@@ -2,18 +2,30 @@
 
 
 #include "SquadPlayerController.h"
+#include "Camera/CameraComponent.h"
+#include "DrawDebugHelpers.h"
 
 void ASquadPlayerController::BeginPlay()
 {
+	if (ASquadPlayerController::GetPawn()) {
+		ControlledPawn = GetPawn();
+		GetPlayerViewPoint(CameraLocation, CameraRotation);
+	}
 }
 
 void ASquadPlayerController::Tick(float DeltatTime)
 {
+	DrawDebugPoint(GetWorld(), CameraLocation, 50, FColor::Red, false, 30, 1);
 }
 
 void ASquadPlayerController::MoveUpCommand(UInputComponent* PlayerInput)
 {
-	if (ASquadPlayerController::Camera) {
+	if (ControlledPawn) {
+
+		
+		//UActorComponent &Camera = GetComponentByClass<ControlledPawn>(UCameraComponent);
 		//Do line trace from camera to first collision; draw a debug line at location.
+		
+		
 	}
 }
--- Source/Squad/Public/SquadPlayerController.h
+++ Source/Squad/Public/SquadPlayerController.h
@@ -17,11 +17,15 @@ class SQUAD_API ASquadPlayerController : public APlayerController
 
 public:
 	//APlayerController();
+	FVector CameraLocation;
+	FRotator CameraRotation;
+	APawn* ControlledPawn;
 
 protected:
 	virtual void BeginPlay() override;
 
 public:
+	
 	virtual void Tick(float DeltatTime) override;
 
 	virtual void MoveUpCommand(class UInputComponent* PlayerInput);
